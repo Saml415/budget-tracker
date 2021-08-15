3295bab0ec048113cd609890639284c18ad85295
Sam Lloyd <bobdoe784@gmail.com>
2021-08-15 12:34:17 -0700

    Fix caching bug

--- public/db.js
+++ public/db.js
@@ -1,6 +1,3 @@
-// TODO: setup indexedDb connection and create an object store for saving
-// transaction data when the user is offline.
-
 let db;
 const budgetVersion = 1;
 
@@ -29,7 +26,6 @@ request.onsuccess = function (evt) {
   console.log("success");
   db = evt.target.result;
 
-  // Check if app is online before reading from db
   if (navigator.onLine) {
     console.log("Backend online! 🗄️");
     checkDatabase();
@@ -49,22 +45,17 @@ function saveRecord(record) {
 function checkDatabase() {
   console.log("check db invoked");
 
-  // Open a transaction on your BudgetStore db
   const transaction = db.transaction(["BudgetStore"], "readonly");
 
-  // access your BudgetStore object
   const store = transaction.objectStore("BudgetStore");
 
-  // Get all records from store and set to a variable
   const getAll = store.getAll();
 
-  // If the request was successful
   getAll.onsuccess = async function () {
     if (getAll.result.length === 0) {
-      // no items to post to backend
       return;
     }
-    // If there are items in the store, we need to bulk add them when we are back online
+
     const response = await fetch("/api/transaction/bulk", {
       method: "POST",
       body: JSON.stringify(getAll.result),
@@ -74,23 +65,16 @@ function checkDatabase() {
       },
     });
     const dbTransactions = await response.json();
-    // If our returned response is not empty
+
     if (dbTransactions.length > 0) {
-      // Open another transaction to BudgetStore with the ability to read and write
       const delTxn = db.transaction(["BudgetStore"], "readwrite");
 
-      // Assign the current store to a variable
       const currentStore = delTxn.objectStore("BudgetStore");
 
-      // Clear existing entries because our bulk add was successful
       currentStore.clear();
       console.log("Clearing store 🧹");
     }
   };
-  // TODO: this function should check for any saved transactions and post them
-  // all to the database. Delete the transactions from IndexedDB if the post
-  // request is successful.
 }
 
-// listen for app coming back online
 window.addEventListener("online", checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,5 @@
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
